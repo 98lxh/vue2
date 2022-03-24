af75377c3c91071795bf981be335507b3084748c
98lxh <582658211@qq.com>
2022-03-24 23:43:06 +0800

    feat:生成渲染函数

--- dist/umd/vue.js
+++ dist/umd/vue.js
@@ -341,9 +341,14 @@
   }
 
   function compileToFunction(template) {
-    var root = parserHTML(template);
-    var code = generate(root);
-    console.log(code);
+    //解析字符串 将html转换成ast语法树
+    var root = parserHTML(template); //将ats语法树转换成js语法
+    //<div id="app"></div> -> _c("div",{id:app},"")
+
+    var code = generate(root); //所有的模板引擎实现都需要new Function + with
+
+    var renderFn = new Function("with(this){return ".concat(code, "}"));
+    return renderFn;
   }
 
   //判断是否为一个对象
--- src/compiler/index.js
+++ src/compiler/index.js
@@ -200,7 +200,12 @@ function generate(el) {
 
 
 export function compileToFunction(template) {
+  //解析字符串 将html转换成ast语法树
   const root = parserHTML(template);
+  //将ats语法树转换成js语法
+  //<div id="app"></div> -> _c("div",{id:app},"")
   let code = generate(root);
-  console.log(code)
+  //所有的模板引擎实现都需要new Function + with
+  let renderFn = new Function(`with(this){return ${code}}`)
+  return renderFn
 }
