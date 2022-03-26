edf2ecfca6a73e3470a9ce350bb542af48caba11
98lxh <582658211@qq.com>
2022-03-26 16:29:49 +0800

    chore:单词写错了

---
+++ src/initGlobalAPI/index.js
@@ -0,0 +1,9 @@
+import { mergeOptions } from "../utils/index"
+export function initGolbalAPI(Vue){
+  //整合了全局相关的内容
+  Vue.options = {}
+
+  Vue.mixin = function(mixin){
+    this.options = mergeOptions(this.options,mixin)
+  }
+}
