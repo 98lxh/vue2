cd6d4873b4c8461b1cf298e6f5dc96c7f84c5f25
98lxh <582658211@qq.com>
2022-03-26 16:52:08 +0800

    feat:对象的依赖收集

---
+++ src/observer/dep.js
@@ -0,0 +1,34 @@
+let id = 0
+export class Dep {
+  constructor(){
+    this.id = id ++
+    this.subs = []
+  }
+  //依赖收集
+  depend(){
+    //观察者模式
+    this.subs.push(Dep.target)
+  }
+
+  //依赖更新
+  notify(){
+    this.subs.forEach(watcher => {
+      watcher.update()
+    })
+  }
+}
+
+//静态属性
+Dep.target
+
+let stack = [];
+//将watcher保留 和移除
+export function pushTarget(watcher){
+  Dep.target = watcher
+  stack.push(watcher)
+}
+
+export function popTarget(){
+  stack.pop();
+  Dep.target = stack[stack.length - 1]
+}
